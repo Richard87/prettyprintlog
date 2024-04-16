b92b928c0ce397fbbcbf21811283410daafb3d9f
Richard87 <richard.hagen@gmail.com>
2024-04-16 12:52:54 +0200

    Added shorthand to options

--- main.go
+++ main.go
@@ -2,31 +2,25 @@ package main
 
 import (
 	"bufio"
-	"flag"
 	"fmt"
 	"os"
 	"time"
 
 	"github.com/rs/zerolog"
-	"github.com/spf13/pflag"
-	"github.com/spf13/viper"
+	flag "github.com/spf13/pflag"
 )
+
+func init() {
+	flag.StringVarP(&zerolog.TimestampFieldName, "timestamp", "t", "time", "Timestamp field name")
+	flag.StringVarP(&zerolog.MessageFieldName, "message", "m", "message", "Message field name")
+	flag.StringVarP(&zerolog.LevelFieldName, "level", "l", "level", "Level field name")
+}
 
 func main() {
 	scanner := bufio.NewScanner(os.Stdin)
-	flag.String("timestamp", "time", "Timestamp field name")
-	flag.String("message", "message", "Message field name")
-	flag.String("level", "level", "Level field name")
-	pflag.CommandLine.AddGoFlagSet(flag.CommandLine)
-	pflag.Parse()
 
-	if err := viper.BindPFlags(pflag.CommandLine); err != nil {
-		panic(err)
-	}
+	flag.Parse()
 
-	zerolog.TimestampFieldName = viper.GetString("timestamp")
-	zerolog.MessageFieldName = viper.GetString("message")
-	zerolog.LevelFieldName = viper.GetString("level")
 	writer := zerolog.ConsoleWriter{Out: os.Stderr, TimeFormat: time.TimeOnly}
 	for scanner.Scan() {
 		content := scanner.Text()
