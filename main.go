dfad7373cdfb352d7411deb04b64c880e24a7360
Richard87 <richard.hagen@gmail.com>
2024-04-16 12:45:49 +0200

    Options to select field names for level, time and message

--- main.go
+++ main.go
@@ -2,15 +2,31 @@ package main
 
 import (
 	"bufio"
+	"flag"
 	"fmt"
 	"os"
 	"time"
 
 	"github.com/rs/zerolog"
+	"github.com/spf13/pflag"
+	"github.com/spf13/viper"
 )
 
 func main() {
 	scanner := bufio.NewScanner(os.Stdin)
+	flag.String("timestamp", "time", "Timestamp field name")
+	flag.String("message", "message", "Message field name")
+	flag.String("level", "level", "Level field name")
+	pflag.CommandLine.AddGoFlagSet(flag.CommandLine)
+	pflag.Parse()
+
+	if err := viper.BindPFlags(pflag.CommandLine); err != nil {
+		panic(err)
+	}
+
+	zerolog.TimestampFieldName = viper.GetString("timestamp")
+	zerolog.MessageFieldName = viper.GetString("message")
+	zerolog.LevelFieldName = viper.GetString("level")
 	writer := zerolog.ConsoleWriter{Out: os.Stderr, TimeFormat: time.TimeOnly}
 	for scanner.Scan() {
 		content := scanner.Text()
